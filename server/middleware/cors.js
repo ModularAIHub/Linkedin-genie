cf136702f20118fe92660f79ff51613a4694d1f2
kanishk2404 <saraswatkanishk24@gmail.com>
2025-10-01 16:58:24 +0530

    vercel spa error

--- server/middleware/cors.js
+++ server/middleware/cors.js
@@ -4,6 +4,11 @@ const allowedOrigins = [
   'http://localhost:5175', // LinkedIn Genie frontend
   'http://localhost:5173', // Platform frontend
   'http://localhost:5174', // Tweet Genie frontend
+  'https://suitegenie.in',
+  'https://tweet.suitegenie.in',
+  'https://linkedin.suitegenie.in',
+  'https://byok.suitegenie.in',
+  'https://api.suitegenie.in',
 ];
 
 const corsOptions = {
